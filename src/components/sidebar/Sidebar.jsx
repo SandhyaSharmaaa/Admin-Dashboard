c3495951ef97095295e90c5f3a062ba5412d6396
sandhya <sandhyasharmavdoit@gmail.com>
2024-03-11 19:12:40 +0530

    fix: name

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -16,12 +16,12 @@ import { Link } from "react-router-dom";
 // import { useContext } from "react";
 
 const Sidebar = () => {
-//   const { dispatch } = useContext(DarkModeContext);
+  //   const { dispatch } = useContext(DarkModeContext);
   return (
     <div className="sidebar">
       <div className="top">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">MyAdminPanel</span>
+          <span className="logo">MyAdmin</span>
         </Link>
       </div>
       <hr />
@@ -89,11 +89,11 @@ const Sidebar = () => {
       <div className="bottom">
         <div
           className="colorOption"
-        //   onClick={() => dispatch({ type: "LIGHT" })}
+          //   onClick={() => dispatch({ type: "LIGHT" })}
         ></div>
         <div
           className="colorOption"
-        //   onClick={() => dispatch({ type: "DARK" })}
+          //   onClick={() => dispatch({ type: "DARK" })}
         ></div>
       </div>
     </div>
