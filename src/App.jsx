224ed31829662ad80aee2b99bd72abfb610f65bf
sandhya <sandhyasharmavdoit@gmail.com>
2024-04-10 20:12:35 +0530

    feat: user page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
-// import New from "./pages/new/New";
+import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { productInputs, userInputs } from "./formSource";
+import { productInputs, userInputs } from "./formSource";
 // import "./style/dark.scss";
 // import { useContext } from "react";
 // import { DarkModeContext } from "./context/darkModeContext";
@@ -22,18 +22,18 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              {/* <Route
+              <Route
                 path="new"
                 element={<New inputs={userInputs} title="Add New User" />}
-              /> */}
+              />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              {/* <Route
+              <Route
                 path="new"
                 element={<New inputs={productInputs} title="Add New Product" />}
-              /> */}
+              />
             </Route>
           </Route>
         </Routes>
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,15 @@
+import Datatable from "../../components/datatable/Datatable";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./list.scss";
 
 export const List = () => {
   return (
-    <div>List</div>
+    <div className="list"><Sidebar />
+    <div className="listContainer"><Navbar />
+    <Datatable/>
+    </div>
+    </div>
   )
 }
 export default List;
